7f099272c70e8b8ef71ceb0f7aa48f13fa3228b0
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2016-12-29 11:03:33 +0530

    Initial Commit

---
+++ placeholderTextMove/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  placeholderTextMove
+//
+//  Created by Tecksky Techonologies on 12/29/16.
+//  Copyright © 2016 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ placeholderTextMove/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  placeholderTextMove
+//
+//  Created by Tecksky Techonologies on 12/29/16.
+//  Copyright © 2016 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
